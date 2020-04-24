4d6b0193285ac73a679ad505958adfb5aeecc450
Biriotti <A116114@bf.arg.igrupobbva>
2020-04-24 13:28:33 -0300

    Arreglos a create-book-repository

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -1,5 +1,7 @@
 package wolox.training.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import wolox.training.exceptions.BookNotFoundException;
 import wolox.training.models.Book;
 import wolox.training.repositories.BookRepository;
 
@@ -19,26 +22,26 @@ public class BookController {
 	@Autowired
 	private BookRepository bookRepository;
 	
-	@GetMapping
+	@GetMapping("/all")
 	public Iterable<Book> findAll() {
 		System.out.println("En BookController -> findAll");
 		return bookRepository.findAll(); 
 	}
 	
-	@GetMapping("/{author}")
-	public ResponseEntity<Object> findByAuthor(@PathVariable String author) {
+	@GetMapping
+	public ResponseEntity<Object> findByAuthor(@RequestParam String author) {
 		System.out.println("En BookController -> findByAuthor");
-		/*SpringData: Sorting with a Sort Parameter */
+		//Sorting with a Sort Parameter 
 		/*
 		Sort sortByYear = Sort.by("year").descending();
 		Book book bookRepository.findByAuthorAndSort(author, sortByYear);
 		*/
-		/*SpringData: Sorting With the OrderBy Method Keyword  */		
+		//Sorting With the OrderBy Method Keyword
 		Book book = bookRepository.findFirstByAuthorOrderByYearDesc(author);
 		if (book == null) {
-			return ResponseEntity.notFound().build();	
+			return ResponseEntity.notFound().build();
 		} else {			
 			return ResponseEntity.ok(book);
-		}		
+		}
 	}
 }
--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,6 +1,7 @@
 package wolox.training.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.constraints.NotBlank;
 
@@ -13,10 +14,10 @@ import wolox.training.models.Book;
 public interface BookRepository extends CrudRepository<Book, Long> {
     List<Book> findByTitle(String title);
     
+    //Comentarios adrede para mantener conocimientos y ayuda memoria de la capacitacion 
     //Book findByAuthor(@NotBlank String author);
-    //Book findFirstByAuthor(@NotBlank String author);
-    Book findFirstByAuthorOrderByYearDesc(@NotBlank String author);
+    //Book findFirstByAuthor(@NotBlank String author);    
     //Book findByAuthorAndSort(@NotBlank String author, Sort sort);    
     //Book findByAuthorOrderByYearDesc(@NotBlank String author);
-    //Optional<Book> findByAuthorOrderByYearDesc(@NotBlank String author);
+    Book findFirstByAuthorOrderByYearDesc(@NotBlank String author);
 }
